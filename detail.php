a5b2ada6287862c3b3c91bf882da1d375f1dfc44
Sofia <sofiamoralesgonzalez1992@gmail.com>
2020-09-13 20:49:46 -0300

    Âform

--- detail.php
+++ detail.php
@@ -150,6 +150,59 @@
                                                 alert(`payment method info error: ${response}`);
                                             }
                                             }
+                                            function getIssuers(paymentMethodId) {
+                                                window.Mercadopago.getIssuers(
+                                                    paymentMethodId,
+                                                    setIssuers
+                                                );
+                                                }
+
+                                                     function setIssuers(status, response) {
+                                                        if (status == 200) {
+                                                        let issuerSelect = document.getElementById('issuer');
+                                                        response.forEach( issuer => {
+                                                        let opt = document.createElement('option');
+                                                        opt.text = issuer.name;
+                                                        opt.value = issuer.id;
+                                                        issuerSelect.appendChild(opt);
+                                                        });
+
+                                                         getInstallments(
+                                                         document.getElementById('paymentMethodId').value,
+                                                        document.getElementById('transactionAmount').value,
+                                                        issuerSelect.value
+                                                         );
+                                                        } else {
+                                                        alert(`issuers method info error: ${response}`);
+                                                         }
+                                                          }
+                                                            doSubmit = false;
+                                        document.getElementById('paymentForm').addEventListener('submit', getCardToken);
+                                        function getCardToken(event){
+                                        event.preventDefault();
+                                        if(!doSubmit){
+                                            let $form = document.getElementById('paymentForm');
+                                            window.Mercadopago.createToken($form, setCardTokenAndPay);
+                                            return false;
+                                        }
+                                        };
+
+                                        function setCardTokenAndPay(status, response) {
+                                        if (status == 200 || status == 201) {
+                                            let form = document.getElementById('paymentForm');
+                                            let card = document.createElement('input');
+                                            card.setAttribute('name', 'token');
+                                            card.setAttribute('type', 'hidden');
+                                            card.setAttribute('value', response.id);
+                                            form.appendChild(card);
+                                            doSubmit=true;
+                                            form.submit();
+                                        } else {
+                                            alert("Verify filled data!\n"+JSON.stringify(response, null, 4));
+                                        }
+                                        };
+
+
 
 
                                 </div>
