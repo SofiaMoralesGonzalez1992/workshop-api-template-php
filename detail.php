cbfb57f0d9cf78737c83eca6fdafc6092e25b1c3
Sofia <sofiamoralesgonzalez1992@gmail.com>
2020-09-13 20:39:01 -0300

    Âform

--- detail.php
+++ detail.php
@@ -119,10 +119,44 @@
                                         
                                         window.Mercadopago.setPublishableKey(TEST-11d1d687-7a23-44d2-b8f2-2883429eb15e);
 
+                                            window.Mercadopago.getIdentificationTypes();
+
+                                            document.getElementById('cardNumber').addEventListener('change', guessPaymentMethod);
+
+                                            function guessPaymentMethod(event) {
+                                            let cardnumber = document.getElementById("cardNumber").value;
+                                            if (cardnumber.length >= 6) {
+                                                let bin = cardnumber.substring(0,6);
+                                                window.Mercadopago.getPaymentMethod({
+                                                    "bin": bin
+                                                }, setPaymentMethod);
+                                            }
+                                            };
+
+                                            function setPaymentMethod(status, response) {
+                                            if (status == 200) {
+                                                let paymentMethod = response[0];
+                                                document.getElementById('paymentMethodId').value = paymentMethod.id;
+
+                                                if(paymentMethod.additional_info_needed.includes("issuer_id")){
+                                                    getIssuers(paymentMethod.id);
+                                                } else {
+                                                    getInstallments(
+                                                        paymentMethod.id,
+                                                        document.getElementById('transactionAmount').value
+                                                    );
+                                                }
+                                            } else {
+                                                alert(`payment method info error: ${response}`);
+                                            }
+                                            }
+
+
                                 </div>
                             </div>
                         </div>
                     </div>
+                    
                 </div>
                 </div>
                 <br><br>
