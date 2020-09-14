1a03f89838aff857884c9711a21d5de75d539de0
Sofia <sofiamoralesgonzalez1992@gmail.com>
2020-09-13 21:14:48 -0300

    front

--- detail.php
+++ detail.php
@@ -115,96 +115,93 @@
                                         </div>
                                     </div>
                                     </form>
-                                      <script type="text/javascript">
+                                        <script type="text/javascript">
                                         
-                                        window.Mercadopago.setPublishableKey(TEST-11d1d687-7a23-44d2-b8f2-2883429eb15e);
+                                            window.Mercadopago.setPublishableKey(TEST-11d1d687-7a23-44d2-b8f2-2883429eb15e);
 
-                                            window.Mercadopago.getIdentificationTypes();
+                                                window.Mercadopago.getIdentificationTypes();
 
-                                            document.getElementById('cardNumber').addEventListener('change', guessPaymentMethod);
+                                                document.getElementById('cardNumber').addEventListener('change', guessPaymentMethod);
 
-                                            function guessPaymentMethod(event) {
-                                            let cardnumber = document.getElementById("cardNumber").value;
-                                            if (cardnumber.length >= 6) {
-                                                let bin = cardnumber.substring(0,6);
-                                                window.Mercadopago.getPaymentMethod({
-                                                    "bin": bin
-                                                }, setPaymentMethod);
+                                                function guessPaymentMethod(event) {
+                                                let cardnumber = document.getElementById("cardNumber").value;
+                                                if (cardnumber.length >= 6) {
+                                                    let bin = cardnumber.substring(0,6);
+                                                    window.Mercadopago.getPaymentMethod({
+                                                        "bin": bin
+                                                    }, setPaymentMethod);
+                                                }
+                                                };
+
+                                                function setPaymentMethod(status, response) {
+                                                if (status == 200) {
+                                                    let paymentMethod = response[0];
+                                                    document.getElementById('paymentMethodId').value = paymentMethod.id;
+
+                                                    if(paymentMethod.additional_info_needed.includes("issuer_id")){
+                                                        getIssuers(paymentMethod.id);
+                                                    } else {
+                                                        getInstallments(
+                                                            paymentMethod.id,
+                                                            document.getElementById('transactionAmount').value
+                                                        );
+                                                    }
+                                                } else {
+                                                    alert(`payment method info error: ${response}`);
+                                                }
+                                                }
+                                                function getIssuers(paymentMethodId) {
+                                                    window.Mercadopago.getIssuers(
+                                                        paymentMethodId,
+                                                        setIssuers
+                                                    );
+                                                    }
+
+                                                        function setIssuers(status, response) {
+                                                            if (status == 200) {
+                                                            let issuerSelect = document.getElementById('issuer');
+                                                            response.forEach( issuer => {
+                                                            let opt = document.createElement('option');
+                                                            opt.text = issuer.name;
+                                                            opt.value = issuer.id;
+                                                            issuerSelect.appendChild(opt);
+                                                            });
+
+                                                            getInstallments(
+                                                            document.getElementById('paymentMethodId').value,
+                                                            document.getElementById('transactionAmount').value,
+                                                            issuerSelect.value
+                                                            );
+                                                            } else {
+                                                            alert(`issuers method info error: ${response}`);
+                                                            }
+                                                            }
+                                                                doSubmit = false;
+                                            document.getElementById('paymentForm').addEventListener('submit', getCardToken);
+                                            function getCardToken(event){
+                                            event.preventDefault();
+                                            if(!doSubmit){
+                                                let $form = document.getElementById('paymentForm');
+                                                window.Mercadopago.createToken($form, setCardTokenAndPay);
+                                                return false;
                                             }
                                             };
 
-                                            function setPaymentMethod(status, response) {
-                                            if (status == 200) {
-                                                let paymentMethod = response[0];
-                                                document.getElementById('paymentMethodId').value = paymentMethod.id;
-
-                                                if(paymentMethod.additional_info_needed.includes("issuer_id")){
-                                                    getIssuers(paymentMethod.id);
-                                                } else {
-                                                    getInstallments(
-                                                        paymentMethod.id,
-                                                        document.getElementById('transactionAmount').value
-                                                    );
-                                                }
+                                            function setCardTokenAndPay(status, response) {
+                                            if (status == 200 || status == 201) {
+                                                let form = document.getElementById('paymentForm');
+                                                let card = document.createElement('input');
+                                                card.setAttribute('name', 'token');
+                                                card.setAttribute('type', 'hidden');
+                                                card.setAttribute('value', response.id);
+                                                form.appendChild(card);
+                                                doSubmit=true;
+                                                form.submit();
                                             } else {
-                                                alert(`payment method info error: ${response}`);
+                                                alert("Verify filled data!\n"+JSON.stringify(response, null, 4));
                                             }
-                                            }
-                                            function getIssuers(paymentMethodId) {
-                                                window.Mercadopago.getIssuers(
-                                                    paymentMethodId,
-                                                    setIssuers
-                                                );
-                                                }
-
-                                                     function setIssuers(status, response) {
-                                                        if (status == 200) {
-                                                        let issuerSelect = document.getElementById('issuer');
-                                                        response.forEach( issuer => {
-                                                        let opt = document.createElement('option');
-                                                        opt.text = issuer.name;
-                                                        opt.value = issuer.id;
-                                                        issuerSelect.appendChild(opt);
-                                                        });
-
-                                                         getInstallments(
-                                                         document.getElementById('paymentMethodId').value,
-                                                        document.getElementById('transactionAmount').value,
-                                                        issuerSelect.value
-                                                         );
-                                                        } else {
-                                                        alert(`issuers method info error: ${response}`);
-                                                         }
-                                                          }
-                                                            doSubmit = false;
-                                        document.getElementById('paymentForm').addEventListener('submit', getCardToken);
-                                        function getCardToken(event){
-                                        event.preventDefault();
-                                        if(!doSubmit){
-                                            let $form = document.getElementById('paymentForm');
-                                            window.Mercadopago.createToken($form, setCardTokenAndPay);
-                                            return false;
-                                        }
-                                        };
-
-                                        function setCardTokenAndPay(status, response) {
-                                        if (status == 200 || status == 201) {
-                                            let form = document.getElementById('paymentForm');
-                                            let card = document.createElement('input');
-                                            card.setAttribute('name', 'token');
-                                            card.setAttribute('type', 'hidden');
-                                            card.setAttribute('value', response.id);
-                                            form.appendChild(card);
-                                            doSubmit=true;
-                                            form.submit();
-                                        } else {
-                                            alert("Verify filled data!\n"+JSON.stringify(response, null, 4));
-                                        }
-                                        };
-
-
-
-
+                                            };
+                                         </script>
                                 </div>
                             </div>
                         </div>
