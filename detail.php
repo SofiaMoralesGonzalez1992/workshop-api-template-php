f6c7274f82c62dbe90280b8b33b07a61bc374ce5
Sofia <sofiamoralesgonzalez1992@gmail.com>
2020-09-13 22:03:01 -0300

    form

--- detail.php
+++ detail.php
@@ -116,7 +116,7 @@
                                     </div>
                                     </form>
                                     
-                                    <script type="text/javascript">
+                                    <script>
                                         
                                         window.Mercadopago.setPublishableKey(TEST-25eac18c-e888-4a54-863f-c86269757a40);
                                              
@@ -126,21 +126,21 @@
 
                                                 function guessPaymentMethod(event) {
                                                 let cardnumber = document.getElementById("cardNumber").value;
-                                                if (cardnumber.length >= 6) {
-                                                    let bin = cardnumber.substring(0,6);
-                                                    window.Mercadopago.getPaymentMethod({
-                                                        "bin": bin
-                                                    }, setPaymentMethod);
+                                                    if (cardnumber.length >= 6) {
+                                                        let bin = cardnumber.substring(0,6);
+                                                        window.Mercadopago.getPaymentMethod({
+                                                            "bin": bin
+                                                        }, setPaymentMethod);
                                                 }
                                                 };
 
                                                 function setPaymentMethod(status, response) {
-                                                if (status == 200) {
-                                                    let paymentMethod = response[0];
-                                                    document.getElementById('paymentMethodId').value = paymentMethod.id;
+                                                    if (status == 200) {
+                                                        let paymentMethod = response[0];
+                                                        document.getElementById('paymentMethodId').value = paymentMethod.id;
 
-                                                    if(paymentMethod.additional_info_needed.includes("issuer_id")){
-                                                        getIssuers(paymentMethod.id);
+                                                        if(paymentMethod.additional_info_needed.includes("issuer_id")){
+                                                            getIssuers(paymentMethod.id);
                                                     } else {
                                                         getInstallments(
                                                             paymentMethod.id,
@@ -181,23 +181,23 @@
                                             document.getElementById('paymentForm').addEventListener('submit', getCardToken);
                                             function getCardToken(event){
                                             event.preventDefault();
-                                            if(!doSubmit){
-                                                let $form = document.getElementById('paymentForm');
-                                                window.Mercadopago.createToken($form, setCardTokenAndPay);
-                                                return false;
+                                                if(!doSubmit){
+                                                    let $form = document.getElementById('paymentForm');
+                                                    window.Mercadopago.createToken($form, setCardTokenAndPay);
+                                                    return false;
                                             }
                                             };
 
                                             function setCardTokenAndPay(status, response) {
-                                            if (status == 200 || status == 201) {
-                                                let form = document.getElementById('paymentForm');
-                                                let card = document.createElement('input');
-                                                card.setAttribute('name', 'token');
-                                                card.setAttribute('type', 'hidden');
-                                                card.setAttribute('value', response.id);
-                                                form.appendChild(card);
-                                                doSubmit=true;
-                                                form.submit();
+                                                if (status == 200 || status == 201) {
+                                                    let form = document.getElementById('paymentForm');
+                                                    let card = document.createElement('input');
+                                                    card.setAttribute('name', 'token');
+                                                    card.setAttribute('type', 'hidden');
+                                                    card.setAttribute('value', response.id);
+                                                    form.appendChild(card);
+                                                    doSubmit=true;
+                                                    form.submit();
                                             } else {
                                                 alert("Verify filled data!\n"+JSON.stringify(response, null, 4));
                                             }
